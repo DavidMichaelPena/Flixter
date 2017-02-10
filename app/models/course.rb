cff2c80c5fa4d1323874773f55dd26eb8499ce90
DavidMichaelPena <alexamenos@gmail.com>
2017-02-10 05:30:28 +0000

    Student Enrollment

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,7 +2,7 @@ class Course < ActiveRecord::Base
   mount_uploader :image, ImageUploader
   belongs_to :user
   has_many :sections
-  has_many :images
+  has_many :enrollments
 
   validates :title, presence: true
   validates :description, presence: true
